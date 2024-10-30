78ca8044133c34a39a92d20e9ae272927351bfeb
PrimeX1 <kiritosinon9@gmail.com>
2024-10-30 09:20:54 +0700

    fixing

--- app/page.js
+++ app/page.js
@@ -4,7 +4,7 @@ export default function Main(){
     return (
        <section>
             <Image
-                src="/profile.jpg"
+                src="/my-portfolio/profile.jpg"
                 alt="profile photo"
                 className="rounded-full bg-gray-100 block lg:mt-5 mt-0 lg:mb-5 mb-10 mx-auto sm:float-right sm:ml-5 sm:mb-5"
                 unoptimized

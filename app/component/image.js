4e87f13de3bc91be99b9e4769d735323a46dd29e
PrimeX1 <kiritosinon9@gmail.com>
2025-01-08 19:29:30 +0700

    style(image): change style

--- app/component/image.js
+++ app/component/image.js
@@ -6,18 +6,17 @@ export const Img = ({images})=>{
         <section>
             <div className="grid grid-cols-2 sm:grid-cols-2 gap-4 my-8">
                 {images.map((image,i)=>(
-                    <div key={i} className="relative aspect-square" >
-                        <a href={image.src} target="_BLANK">
+                    // <div key={i} className="relative aspect-square" >
+                        <a key={i} href={image.src} target="_BLANK">
                             <Image
                                 alt="image"
                                 src={image.src}
-                                fill
-                                sizes="100vw"
-                                priority
+                                width={500}
+                                height={500}
                                 className="rounded-lg object-cover transition-transform duration-300 ease-in-out hover:scale-105"
                             />
                         </a>
-                    </div>
+                    // </div>
                 ))}
             </div>
         </section>
